3ea8bbba1767465a64298bcdd1f26396d987133f
Andrei Kushner <andrei.k@playgoals.com>
2025-03-15 18:44:01 +0100

    added argument parsing for debug mode
    * added command line argument parsing
    * changed default logging to INFO
    * added --debug argument to switch logging to DEBUG
    * simplified string format  in INFO mode

--- core/logging.py
+++ core/logging.py
@@ -4,22 +4,30 @@ import logging
 # Define log level type for type hints
 LogLevel = t.Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
+LOG_LEVELS = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL
+}
 
-def setup_logging(
-        level: LogLevel = "INFO",
-        format_str: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-) -> None:
+def setup_logging(level: LogLevel = "INFO",) -> None:
     """
     Configure the root logger with console output at the specified level.
     Call this once at the start of your application.
 
-    Args:
-        level: Root logging level
-        format_str: Format for log messages
+    :param level: Root logging level
+    :return:
     """
+    log_level = LOG_LEVELS.get(level, logging.INFO)
+
+    # Select log format
+    format_str = "[%(levelname)s] %(message)s" if log_level > logging.DEBUG else "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
     # Configure root logger
     root_logger = logging.getLogger()
-    root_logger.setLevel(getattr(logging, level))
+    root_logger.setLevel(log_level)
 
     # Clear existing handlers
     for handler in root_logger.handlers[:]:
@@ -30,7 +38,7 @@ def setup_logging(
     handler.setFormatter(logging.Formatter(format_str))
     root_logger.addHandler(handler)
 
-    logging.debug("Logging configured")  # This will show if level is DEBUG
+    logging.debug("Logging configured")
 
 
 def get_logger(name: str, level: t.Optional[LogLevel] = None) -> logging.Logger:
--- processors/sdf/run.py
+++ processors/sdf/run.py
@@ -1,9 +1,16 @@
+import argparse
 import core.logging as logging
 import processors.sdf.processor as processor
 
 
 def main() -> None:
-    logging.setup_logging(level="DEBUG")
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="SDF Processor")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    args = parser.parse_args()
+
+    log_level:logging.LogLevel = "DEBUG" if args.debug else "INFO"
+    logging.setup_logging(level=log_level)
     logger = logging.get_logger(__name__)
 
     processor.process_sdf()
